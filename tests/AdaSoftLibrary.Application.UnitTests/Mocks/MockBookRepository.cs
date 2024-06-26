d52390f22ddf5bd6f5ea2a9803360718c84f6185
Peter Púčik <ppucik@gmail.com>
2024-06-26 10:15:40 +0200

    Bug fix Unit test

--- tests/AdaSoftLibrary.Application.UnitTests/Mocks/MockBookRepository.cs
+++ tests/AdaSoftLibrary.Application.UnitTests/Mocks/MockBookRepository.cs
@@ -114,6 +114,7 @@ public class MockBookRepository
 
         mockBookRepository
             .Setup(x => x.GetAuthorsAsync(
+                It.IsAny<string?>(),
                 It.IsAny<CancellationToken>()))
             .ReturnsAsync(() =>
             {
