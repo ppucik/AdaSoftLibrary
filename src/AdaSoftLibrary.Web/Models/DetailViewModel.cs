4748e092c63f5253037ddf977d4f9e8d3ad4085f
Peter Púčik <ppucik@gmail.com>
2024-07-01 10:42:00 +0200

    DetailViewModel bug fix

--- src/AdaSoftLibrary.Web/Models/DetailViewModel.cs
+++ src/AdaSoftLibrary.Web/Models/DetailViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AdaSoftLibrary.Domain.Constants;
+using System.ComponentModel.DataAnnotations;
 
 namespace AdaSoftLibrary.Web.Models;
 
@@ -19,18 +20,23 @@ public class DetailViewModel : BookViewModel
     /// Meno
     /// </summary>
     [Display(Name = "Meno")]
+    [Required(ErrorMessage = MessageConstants.FirstNameCannotBeEmpty)]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = MessageConstants.FirstNameOutOfRange)]
     public string? FirstName { get; set; } = null!;
 
     /// <summary>
     /// Priezvisko
     /// </summary>
     [Display(Name = "Priezvisko")]
+    [Required(ErrorMessage = MessageConstants.LastNameCannotBeEmpty)]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = MessageConstants.LastNameOutOfRange)]
     public string? LastName { get; set; } = null!;
 
     /// <summary>
     /// Dátum požičania
     /// </summary>
     [Display(Name = "Dátum požičania")]
+    [DataType(DataType.Date)]
     public DateOnly? FromDate { get; set; }
 
     /// <summary>
